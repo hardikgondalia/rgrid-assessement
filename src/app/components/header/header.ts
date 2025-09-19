2ec0b26ae6bbe20745ee108b4c0bd120d58c3a3d
Hardik Gondalia <hardikgondalia1990@gmail.com>
2025-09-19 16:53:05 +0530

    Component Create & Design

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeroSection } from "./hero-section/hero-section";
+import { QuickLink } from "./quick-link/quick-link";
+import { BannerSection } from "./banner-section/banner-section";
+import { ProductGrid } from "./product-grid/product-grid";
+import { Product } from "./product/product";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [HeroSection, QuickLink, BannerSection, ProductGrid, Product],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
