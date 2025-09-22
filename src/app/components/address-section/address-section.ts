02d525c147abf65d303227731722f138a28d8938
Hardik Gondalia <hardikgondalia1990@gmail.com>
2025-09-22 15:32:47 +0530

    address and quick links create array

--- src/app/components/address-section/address-section.ts
+++ src/app/components/address-section/address-section.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-address-section',
   imports: [],
   templateUrl: './address-section.html',
-  styleUrl: './address-section.scss'
+  styleUrl: './address-section.scss',
 })
 export class AddressSection {
-
+  addresses = [
+    {
+      image: 'assets/images/address-section/shipping.svg',
+      title: 'Free Shipping',
+      subtitle: 'Free Shipping on Orders Over $59 or with Your First Purchase',
+    },
+    {
+      image: 'assets/images/address-section/policy.svg',
+      title: '30 Days Return policy',
+      subtitle: 'Hassle-free refunds for any issues with the orders',
+    },
+    {
+      image: 'assets/images/address-section/support.svg',
+      title: '24/7 Support',
+      subtitle: 'Our dedicated team is always here to help',
+    },
+  ];
 }
--- src/app/components/quick-link/quick-link.ts
+++ src/app/components/quick-link/quick-link.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-quick-link',
   imports: [],
   templateUrl: './quick-link.html',
-  styleUrl: './quick-link.scss'
+  styleUrl: './quick-link.scss',
 })
 export class QuickLink {
-
+  quickLinks = [
+    {
+      title: '99% Customer Satisfaction',
+      subtitle: 'Rated highly for quality & reliability',
+      active: false,
+    },
+    {
+      title: 'Next-Day Dispatch',
+      subtitle: 'Quick, dependable shipping across the UK',
+      active: false,
+    },
+    {
+      title: 'Safe & Certified',
+      subtitle: 'All products meet medical safety standards',
+      active: true,
+    },
+  ];
 }
