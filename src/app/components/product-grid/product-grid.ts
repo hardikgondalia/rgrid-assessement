3965053323293b122337a787e8796d20d635d781
Hardik Gondalia <hardikgondalia1990@gmail.com>
2025-09-22 15:15:30 +0530

    product page create array

--- src/app/components/product-grid/product-grid.ts
+++ src/app/components/product-grid/product-grid.ts
@@ -1,13 +1,51 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ProductSlider } from "../product-slider/product-slider";
+import { ProductSlider } from '../product-slider/product-slider';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-grid',
   standalone: true,
   templateUrl: './product-grid.html',
   styleUrl: './product-grid.scss',
-  imports: [ProductSlider],
+  imports: [ProductSlider, CommonModule],
 })
 export class ProductGrid {
-  
+  productGridData = {
+    leftSlider: true,
+    rightColumn: {
+      mainCard: {
+        image: 'assets/images/product-grid/product-grid-two.png',
+        alt: 'Stethoscope',
+        imageClass: 'object-fit-cover stethoscope-image',
+        info: {
+          title: 'Save 30% on',
+          subtitle: 'Digital Electronic<br>Stethoscopes',
+          link: { label: 'VISIT STORE', url: '' },
+        },
+      },
+      smallCards: [
+        {
+          image: 'assets/images/product-grid/product-grid-three.png',
+          alt: 'Thermometer',
+          imageClass: 'object-fit-cover thermometer-image',
+          info: {
+            subtitle: 'Get Digital<br>Thermometers',
+            align: 'end',
+            link: { label: 'VISIT STORE', url: '' },
+          },
+        },
+        {
+          image: 'assets/images/product-grid/product-grid-four.png',
+          alt: 'Medical Supply',
+          imageClass: 'object-fit-cover medical-supply-image',
+          extraClass: 'medical-supply-card',
+          info: {
+            subtitle: 'Essential<br>Medical<br>Supplies',
+            align: 'start',
+            link: { label: 'Shop Now', url: '', textLight: true },
+          },
+        },
+      ],
+    },
+  };
 }
--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-product',
   imports: [],
   templateUrl: './product.html',
-  styleUrl: './product.scss'
+  styleUrl: './product.scss',
 })
 export class Product {
-
+  productData = {
+    title: 'Medical & Wellness Products',
+    subtitle: 'Free Shipping on Orders Over $59 or with Your First Purchase',
+    tabs: [
+      { label: 'Popular Items', active: true },
+      { label: 'Top Rated', active: false },
+      { label: 'Trending Items', active: false },
+    ],
+  };
 }
